5397716e8edac24b7a056e31f53ceba555944ce2
Luigi Denora <ldenora@codarchitects.com>
2024-10-29 11:53:51 +0100

    add switcher for models

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,31 @@
-import {
-  Cache,
-  Scene,
-  DirectionalLight,
-  AmbientLight,
-  Color,
-  PointLight,
-} from "three";
+import { Cache, Scene, DirectionalLight, AmbientLight } from "three";
+import GUI from "lil-gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { Main, Asset, OrthographicCameraAuto } from "@three.ez/main";
-import { Soldier } from "./soldier";
+import { Model } from "./soldier";
 
 const canvas = document.getElementById("avatar");
 const main = new Main({ rendererParameters: { canvas }, fullscreen: true }); // init three.ez app
 Cache.enabled = true;
-await Asset.preloadAllPending({
-  onProgress: (e) =>
-    (document.getElementById("loading").textContent = e * 100 + "%"),
-});
+
+const model = new Model("models/cutegirl_2k.glb");
+const gui = new GUI();
+let obj = { model: "models/cutegirl_2k.glb" };
+gui
+  .add(obj, "model", [
+    "models/character_free_model_3d_by_oscar_creativo.glb",
+    "models/cutegirl_1k.glb",
+    "models/cutegirl_2k.glb",
+    "models/lady_deadpool.glb",
+  ])
+  .name("Change Model")
+  .onChange((value: string) => {
+    model.change(value);
+  });
 
 const scene = new Scene().add(
-  new Soldier().translateY(-1.5),
+  model,
   new DirectionalLight(0xffffff, 1.5).translateZ(5),
-  //new PointLight(0xffaa55, 1.5, 100).translateZ(5).translateX(5).translateY(10),
-  //new DirectionalLight(0xffaa55).translateZ(3).translateX(-3),
   new AmbientLight(0xffffff, 1.0),
 );
 const camera = new OrthographicCameraAuto(2).translateZ(100);
--- src/soldier.ts
+++ src/soldier.ts
@@ -1,55 +1,44 @@
-import {
-  AnimationAction,
-  AnimationMixer,
-  Group,
-  Material,
-  MeshStandardMaterial,
-  SkinnedMesh,
-} from "three";
-import { Asset, Utils } from "@three.ez/main";
-import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { AnimationAction, AnimationMixer, Group } from "three";
+import { Asset } from "@three.ez/main";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { clone } from "three/examples/jsm/utils/SkeletonUtils";
 
-Asset.preload(
-  GLTFLoader,
-  //"models/character_free_model_3d_by_oscar_creativo.glb",
-  //"models/cutegirl_1k.glb",
-  "models/cutegirl_2k.glb",
-  //"models/lady_deadpool.glb",
-  //"models/facial_animation.glb",
-);
+export class Model extends Group {
+  change(assetName: string) {
+    if (this.children.length) this.remove(...this.children);
+    Asset.load<any>(GLTFLoader, assetName, ({ loaded, total }) => {
+      document.getElementById("loading").textContent =
+        "download del modello: " + Math.floor((loaded / total) * 100) + "%";
+    }).then((gltf) => {
+      document.getElementById("loading").textContent = "";
 
-export class Soldier extends Group {
-  private _mixer = new AnimationMixer(this);
+      this.add(...clone(gltf.scene).children);
+
+      if (assetName.includes("2k")) {
+        this.translateY(-1.5);
+        this.rotateY(Math.PI);
+      }
+      if (assetName.includes("free")) {
+        this.translateZ(-2.5);
+        this.translateY(-1.5);
+        this.rotateY(Math.PI);
+      }
+
+      if (assetName.includes("lady")) {
+        this.rotateY(Math.PI);
+      }
+      this._mixer = new AnimationMixer(this);
+      this._idleAction = this._mixer.clipAction(gltf.animations[0]);
+
+      this.on("animate", (e) => this._mixer.update(e.delta));
+
+      this._idleAction.play();
+    });
+  }
+  private _mixer: AnimationMixer;
   private _idleAction: AnimationAction;
-  constructor() {
+  constructor(assetName: string) {
     super();
-    const gltf = Asset.get<GLTF>(
-      "models/cutegirl_2k.glb",
-
-      //"models/character_free_model_3d_by_oscar_creativo.glb",
-      //"models/cutegirl_1k.glb",
-
-      //"models/lady_deadpool.glb",
-    );
-    //const redMaterial = new MeshStandardMaterial({ color: 0xff0000 });
-    //console.log(gltf.scene);
-    //gltf.scene.traverse((obj: any) => {
-    //  if (obj.material && obj.material.name === "Hair_A_Mat_Transparency") {
-    //    obj.material = redMaterial;
-    //    debugger;
-    //  }
-    //});
-    this.add(...clone(gltf.scene).children);
-
-    this.rotateY(Math.PI);
-    //Utils.computeBoundingSphereChildren(this); // to make raycast works properly
-    //Utils.setChildrenDragTarget(this, this); // make it draggable
-
-    this._idleAction = this._mixer.clipAction(gltf.animations[0]);
-
-    this.on("animate", (e) => this._mixer.update(e.delta));
-
-    this._idleAction.play();
+    this.change(assetName);
   }
 }
